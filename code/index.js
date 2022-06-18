f166c23b0cdcbaef414ac692e982969f7913fc87
Euglen Mulaj <euglen.mulaj@dunegroup.it>
2022-06-18 16:38:25 +0200

    file upload in progress phase 1

--- code/index.js
+++ code/index.js
@@ -8,17 +8,24 @@ const { response } = require("express");
 const moment = require("moment");
 //session library node js
 const session = require("express-session");
-const path = require('path');
+const path = require("path");
 //encrypting password
 const bcrypt = require("bcrypt");
-
+// excel library
+const reader = require("xlsx");
+//multer library to handle file upload
+const multer = require('multer');
+const upload = multer({ dest: "src/uploads" });
+const uploadConfig = upload.single("uploadFile"); 
+const fs = require('fs');
 
 
 const app = express();
 const port = process.env.PORT || 8888;
 
 // connection to heroku
-const mySQLString = 'mysql://b9a42c0a1b8343:7ec8508b@eu-cdbr-west-02.cleardb.net/heroku_252e1419c201912?reconnect=true';
+const mySQLString =
+  "mysql://b9a42c0a1b8343:7ec8508b@eu-cdbr-west-02.cleardb.net/heroku_252e1419c201912?reconnect=true";
 //connection to local db - mysql
 // const mySQLString =
 //   "mysql://root:asperina123@localhost/new_test_db?reconnect=true";
@@ -35,94 +42,92 @@ app
   )
 
   .use(morgan("dev"))
-//   .use('/',express.static("public"))
-    .use('/static', express.static(path.join(__dirname, 'static')))
+  //   .use('/',express.static("public"))
+  .use("/static", express.static(path.join(__dirname, "static")))
   //parse application/x-www-form-urlencoded
   .use(bodyParser.urlencoded({ extended: false }))
 
   //parse application/json
   .use(bodyParser.json())
+  .use(uploadConfig)
+  //   .get('/login', function(request, response) {
+  // 	// Render login template
+  // 	response.sendFile(path.join(__dirname+'/public' + '/index.html'));
+  // })
 
-
-//   .get('/login', function(request, response) {
-// 	// Render login template
-// 	response.sendFile(path.join(__dirname+'/public' + '/index.html'));
-// })
-
-
-// to load dashboard when logged in 
-.get("/", (req, res) => {
-	// If the user is loggedin
-	if (!req.session.loggedin) {
-		// Output username
-		res.sendFile(path.join(__dirname+'/public' + '/index.html'));
-	} else {
-		// logged in
-        res.redirect('/dashboard');
-	}
-
+  // to load dashboard when logged in
+  .get("/", (req, res) => {
+    // If the user is loggedin
+    if (!req.session.loggedin) {
+      // Output username
+      res.sendFile(path.join(__dirname + "/public" + "/index.html"));
+    } else {
+      // logged in
+      res.redirect("/dashboard");
+    }
   })
 
-.post('/auth',async function(request, response) {
-	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		let results = await database.query('SELECT * FROM accounts WHERE username = @username', 
-        {username: username, password: password});
-			if (results.length > 0) {
-                console.log(results);
-                const validPassword = await bcrypt.compare(password, results[0].password);
-                console.log(validPassword);
-                if(validPassword){
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
-				response.redirect('/dashboard');
-                // response.sendFile(path.join(__dirname+'/public' + '/index.html'));
-                }else{
-                    response.send('Incorrect Username and/or Password!');
+  .post("/auth", async function (request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+      // Execute SQL query that'll select the account from the database based on the specified username and password
+      let results = await database.query(
+        "SELECT * FROM accounts WHERE username = @username",
+        { username: username, password: password }
+      );
+      if (results.length > 0) {
+        console.log(results);
+        const validPassword = await bcrypt.compare(
+          password,
+          results[0].password
+        );
+        console.log(validPassword);
+        if (validPassword) {
+          // Authenticate the user
+          request.session.loggedin = true;
+          request.session.username = username;
+          // Redirect to home page
+          response.redirect("/dashboard");
+          // response.sendFile(path.join(__dirname+'/public' + '/index.html'));
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+      } else {
+        response.send("Incorrect Username and/or Password!");
+      }
+      response.end();
+    } else {
+      response.send("Please enter Username and Password!");
+      response.end();
+    }
+  })
 
-                }
+  .get("/dashboard", function (request, response) {
+    // If the user is loggedin
+    if (request.session.loggedin) {
+      // Output username
+      response.sendFile(path.join(__dirname + "/public" + "/dashboard.html"));
+    } else {
+      // Not logged in
+      response.send("Please login to view this page!");
+    }
+    // response.end();
+  })
 
-            } else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-})
-
-.get('/dashboard', function(request, response) {
-	// If the user is loggedin
-	if (request.session.loggedin) {
-		// Output username
-		response.sendFile(path.join(__dirname+'/public' + '/dashboard.html'));
-	} else {
-		// Not logged in
-		response.send('Please login to view this page!');
-	}
-	// response.end();
-})
-
-.get('/register', function(request, response) {
-	// If the user is loggedin
-	if (!request.session.loggedin) {
-		// Output username
-		response.sendFile(path.join(__dirname+'/public' + '/register.html'));
-	} else {
-		// logged in
-		response.sendFile(path.join(__dirname+'/public' + '/dashboard.html'));
-	}
-	// response.end();
-})
-
+  .get("/register", function (request, response) {
+    // If the user is loggedin
+    if (!request.session.loggedin) {
+      // Output username
+      response.sendFile(path.join(__dirname + "/public" + "/register.html"));
+    } else {
+      // logged in
+      response.sendFile(path.join(__dirname + "/public" + "/dashboard.html"));
+    }
+    // response.end();
+  })
 
   .post("/api/user", async (req, res) => {
     const body = req.body;
@@ -178,10 +183,10 @@ app
   .get("/detail", async (req, res) => {
     if (!req.session.loggedin) {
       // Output username
-      res.sendFile(path.join(__dirname+'/public' + '/register.html'));
+      res.sendFile(path.join(__dirname + "/public" + "/register.html"));
     } else {
       // logged in
-      res.sendFile(path.join(__dirname+'/public' + '/detail.html'));
+      res.sendFile(path.join(__dirname + "/public" + "/detail.html"));
     }
   })
 
@@ -245,7 +250,6 @@ app
 
     const encryptedPassword = await bcrypt.hash(plainPassword, salt);
 
-
     await database.execute(
       `
             INSERT INTO accounts (
@@ -266,23 +270,57 @@ app
     );
     req.session.loggedin = true;
     req.session.username = body.username;
-	res.redirect('/dashboard');
+    res.redirect("/dashboard");
     res.end();
-
   })
 
   .post("/logout", (req, res) => {
-
     req.session.loggedin = false;
-    req.session.username = '';
-	res.redirect('/');
+    req.session.username = "";
+    res.redirect("/");
     res.end();
+  })
+  //post to upload file
+  .post("/upload", (req, res) => {
+    // Reading our test file
+    console.log(req.file);
+    var tmp_path = req.file.path;
+
+  /** The original name of the uploaded file
+      stored in the variable "originalname". **/
+  var target_path = 'src/uploads/' + req.file.originalname;
+  var src = fs.createReadStream(tmp_path);
+  var dest = fs.createWriteStream(target_path);
+  src.pipe(dest);
+  // src.on('end', function() { res.render('complete'); });
+  // src.on('error', function(err) { res.render('error'); });
+  console.log(target_path);
+
+    const file = reader.readFile('./' + target_path);
+    let data = [];
+
+    const sheets = file.SheetNames;
+    console.log('sheets: ' + sheets);
+
+    for (let i = 0; i < sheets.length; i++) {
+      const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+      console.log('sheet1: ' + file.Sheets);
+      console.log('TEMP: ' + temp);
+      temp.forEach((res) => {
+        data.push(res);
+      });
+    }
+
+    // Printing data
+    console.log(data);
+
+    res.redirect("/dashboard");
 
   })
 
-  .use(function(req, res, next){
+  .use(function (req, res, next) {
     res.status(404);
-    res.send('Not found');
+    res.send("Not found");
   })
 
   .listen(port, () => console.log(`Server listening on port ${port}`));
